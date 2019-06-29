193f981954db6c47e5ba11e28e569f496c3a7357
tvaliasek <tomas@valiasek.cz>
2019-06-29 22:59:15 +0200

    nette 3

--- src/Bridges/NetteDI/FormsExtension.php
+++ src/Bridges/NetteDI/FormsExtension.php
@@ -12,32 +12,30 @@ namespace Tvaliasek\NextrasFormsFork\Bridges\NetteDI;
 use Nette\DI\CompilerExtension;
 use Nette\Forms\Container;
 use Nette\PhpGenerator\ClassType;
-use Nette\Utils\ObjectMixin;
 use Tvaliasek\NextrasFormsFork\Controls;
 
 
 class FormsExtension extends CompilerExtension
 {
-	public function beforeCompile()
-	{
-		parent::beforeCompile();
-	}
+    public function afterCompile(ClassType $class): void
+    {
+        $init = $class->getMethod('initialize');
+        $init->addBody(__CLASS__ . '::registerControls();');
+    }
 
-
-	public function afterCompile(ClassType $class)
-	{
-		$init = $class->getMethods()['initialize'];
-		$init->addBody(__CLASS__ . '::registerControls();');
-	}
-
-
-	public static function registerControls()
-	{
-		ObjectMixin::setExtensionMethod(Container::class, 'addDatePicker', function (Container $container, $name, $label = null) {
-			return $container[$name] = new Controls\DatePicker($label);
-		});
-		ObjectMixin::setExtensionMethod(Container::class, 'addDateTimePicker', function (Container $container, $name, $label = null) {
-			return $container[$name] = new Controls\DateTimePicker($label);
-		});
-	}
+    public static function registerControls(): void
+    {
+        Container::extensionMethod('addDatePicker',
+            function ($form, $name, $label = null) {
+                $form[$name] = new Controls\DatePicker($label);
+                return $form[$name];
+            }
+        );
+        Container::extensionMethod('addDateTimePicker',
+            function ($form, $name, $label = null) {
+                $form[$name] = new Controls\DatePicker($label);
+                return $form[$name];
+            }
+        );
+    }
 }
